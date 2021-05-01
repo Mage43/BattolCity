c1bd5190693e163a964384ea4f36d9da724a50f9
Mage <fom400313@gmail.com>
2021-05-01 20:58:00 +0300

    OpenGL и glfw было дабавлено размеры окна и овтомизация , и окено закрываеться с помощи нажатея клавиотуры ESC

--- исходник/main.cpp
+++ исходник/main.cpp
@@ -1,33 +1,69 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+int g_windowРазмерX = 640;
+int g_windowРазмерY = 480;
+
+void glfwОбратноеВызовРазмераОкна(GLFWwindow* window, int шрена, int высота)
+{
+    g_windowРазмерX = шрена;
+    g_windowРазмерY = высота;
+    glViewport(0, 0, g_windowРазмерX, g_windowРазмерY);
+}
+
+void glfwОбратноеВызовНажатие(GLFWwindow* window, int клава, int сконированиеКода, int действовать, int режим)
+{
+    if (клава == GLFW_KEY_ESCAPE && сконированиеКода == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+
 int main(void)
 {
-    GLFWwindow* window;
+    setlocale(LC_ALL, "rus");
+
+   
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "не отрылось файл" << std::endl;
         return -1;
-
+    }  
+    /*Версия major,minor и  функциия опинчии */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+      GLFWwindow* window = glfwCreateWindow(g_windowРазмерX, g_windowРазмерY, "BattolCeti", nullptr, nullptr);
     if (!window)
     {
+        std::cout << "ошибка при открытия окна " << std::endl;
         glfwTerminate();
         return -1;
     }
+    glfwSetWindowSizeCallback(window, glfwОбратноеВызовРазмераОкна);
+    glfwSetKeyCallback(window, glfwОбратноеВызовНажатие);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 	
 	if (!gladLoadGL())
 	{
-		std::cout << "Can`t load GLAD" << std::endl;
+		std::cout << "Не могу загрузить GLAD" << std::endl;
 		return -1;
 	}
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    /*вод ркндор версия OpenGL */
+    std::cout << "Рендор: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL версия: " << glGetString(GL_VERSION) << std::endl;
+
 	
-	glClearColor(1, 1, 1, 1);
+	
+	glClearColor(1, 1, 0, 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
