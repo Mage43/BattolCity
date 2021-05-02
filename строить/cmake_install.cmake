79703682d9c21b438b111a560ce2a7dd0c735d1c
Mage <fom400313@gmail.com>
2021-05-02 21:34:42 +0300

    OponGL построен треугольник

--- строить/cmake_install.cmake
+++ строить/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/BattolCity/BattolCity/строить/внешний/glfw/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/BattolCity/BattolCity/строить/внешний/glad/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
