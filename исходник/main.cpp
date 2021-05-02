8c0af70bf3e42721a1f5afd288804ac598dd44b6
Mage <fom400313@gmail.com>
2021-05-02 21:29:20 +0300

    OponGL построен треугольник

--- исходник/main.cpp
+++ исходник/main.cpp
@@ -3,6 +3,39 @@
 
 #include <iostream>
 
+GLfloat точка[] = {
+  0.0f, 0.5f, 0.0f,
+  0.5f,-0.5f, 0.0f,
+ -0.5f,-0.5f, 0.0f
+};
+
+GLfloat цвет[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* вершинный_шейдер =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main() {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* фрагментный_шейдер =
+"#version 460\n"
+"in vec3 color ;"
+"out vec4 frag_color;"
+"void main() {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
+
+
+
 int g_windowРазмерX = 640;
 int g_windowРазмерY = 480;
 
@@ -26,20 +59,20 @@ int main(void)
 {
     setlocale(LC_ALL, "rus");
 
-   
+
 
     /* Initialize the library */
     if (!glfwInit())
     {
         std::cout << "не отрылось файл" << std::endl;
         return -1;
-    }  
+    }
     /*Версия major,minor и  функциия опинчии */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-      GLFWwindow* window = glfwCreateWindow(g_windowРазмерX, g_windowРазмерY, "BattolCeti", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(g_windowРазмерX, g_windowРазмерY, "BattolCeti", nullptr, nullptr);
     if (!window)
     {
         std::cout << "ошибка при открытия окна " << std::endl;
@@ -51,25 +84,73 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-	
-	if (!gladLoadGL())
-	{
-		std::cout << "Не могу загрузить GLAD" << std::endl;
-		return -1;
-	}
+
+    if (!gladLoadGL())
+    {
+        std::cout << "Не могу загрузить GLAD" << std::endl;
+        return -1;
+    }
     /*вод ркндор версия OpenGL */
     std::cout << "Рендор: " << glGetString(GL_RENDERER) << std::endl;
     std::cout << "OpenGL версия: " << glGetString(GL_VERSION) << std::endl;
 
-	
-	
-	glClearColor(1, 1, 0, 1);
+
+
+    glClearColor(1, 1, 0, 1);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &вершинный_шейдер, nullptr);
+    glCompileShader(vs);
+
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &фрагментный_шейдер, nullptr);
+    glCompileShader(fs);
+
+
+    GLuint шейдерная_программа = glCreateProgram();
+    glAttachShader(шейдерная_программа, vs);
+    glAttachShader(шейдерная_программа, fs);
+    glLinkProgram(шейдерная_программа);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint точка_vbo = 0;
+    glGenBuffers(1, &точка_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(точка), точка, GL_STATIC_DRAW);
+
+
+
+    GLuint цвет_vbo = 0;
+    glGenBuffers(1, &цвет_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(цвет), цвет, GL_STATIC_DRAW);
+
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glUseProgram(шейдерная_программа);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
