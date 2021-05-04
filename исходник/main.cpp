24e39a54f1cebbef9a2788cd8930f3e26d4c0874
Mage <fom400313@gmail.com>
2021-05-04 19:00:00 +0300

    Создон класc Ресурсный Менеджер

--- исходник/main.cpp
+++ исходник/main.cpp
@@ -4,6 +4,7 @@
 #include "–ендор/Ўейдерна€ѕрограмма.h"
 
 #include <iostream>
+#include "–есерсы/–есурсныйћенеджер.h"
 
 GLfloat точка[] = {
   0.0f, 0.5f, 0.0f,
@@ -16,26 +17,6 @@ GLfloat цвет[] = {
     0.0f, 1.0f, 0.0f,
     0.0f, 0.0f, 1.0f
 };
-
-const char* вершинный_шейдер =
-"#version 460\n"
-"layout(location = 0) in vec3 vertex_position;"
-"layout(location = 1) in vec3 vertex_color;"
-"out vec3 color;"
-"void main() {"
-"   color = vertex_color;"
-"   gl_Position = vec4(vertex_position, 1.0);"
-"}";
-
-const char* фрагментный_шейдер =
-"#version 460\n"
-"in vec3 color ;"
-"out vec4 frag_color;"
-"void main() {"
-"   frag_color = vec4(color, 1.0);"
-"}";
-
-
 
 
 int g_window–азмерX = 640;
@@ -57,9 +38,11 @@ void glfwќбратное¬ызовЌажатие(GLFWwindow* window, int клава, int сконирование од
 }
 
 
-int main(void)
+int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "rus");
+
+   
 
 
 
@@ -100,59 +83,59 @@ int main(void)
 
     glClearColor(1, 1, 0, 1);
 
-    std::string ¬ершинныйЎейдер(вершинный_шейдер);
-    std::string ‘рагментныйѕрограмма(фрагментный_шейдер);
-    –ендор::Ўейдерна€ѕрограмма шейдерна€ѕрограмма(¬ершинныйЎейдер, ‘рагментныйѕрограмма);
+    //наход€м расположение папки исполнитель
     
-    if (!шейдерна€ѕрограмма.воз апел€ции())
-    {
-        std::cerr << "Ќе могу создать Ўейдерна€ѕрограмма " << std::endl;
+    {  –есурсныйћенеджер ресурсныйћенеджер(argv[0]);
+        auto загрузитьЎейдонуюѕрограму = ресурсныйћенеджер.загрускаЎедоры("загрускаЎедоры", "ресерсы/Ўейдор/вершина.txt", "ресерсы/Ўейдор/фрогмент.txt");
+       if (!загрузитьЎейдонуюѕрограму)
+       {
+        std::cerr << "Ќе могу создать Ўейдерна€ ѕрограмма: "<<"загрускаЎедоры" << std::endl;
         return -1;
-    }
-
-    GLuint точка_vbo = 0;
-    glGenBuffers(1, &точка_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(точка), точка, GL_STATIC_DRAW);
-
-
-
-    GLuint цвет_vbo = 0;
-    glGenBuffers(1, &цвет_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(цвет), цвет, GL_STATIC_DRAW);
+       }
+       
+        GLuint точка_vbo = 0;
+        glGenBuffers(1, &точка_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(точка), точка, GL_STATIC_DRAW);
 
 
-    GLuint vao = 0;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
 
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        GLuint цвет_vbo = 0;
+        glGenBuffers(1, &цвет_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(цвет), цвет, GL_STATIC_DRAW);
 
 
-        шейдерна€ѕрограмма.вкл();
+        GLuint vao = 0;
+        glGenVertexArrays(1, &vao);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, точка_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, цвет_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+
+            загрузитьЎейдонуюѕрограму->вкл();
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
     }
-
     glfwTerminate();
     return 0;
 }
---
+++ исходник/Ресерсы/РесурсныйМенеджер.h
@@ -0,0 +1,40 @@
+#pragma once 
+
+#include<string>
+#include<memory>
+#include<map>
+
+namespace –ендор
+{
+	class Ўейдерна€ѕрограмма;
+}
+
+
+class –есурсныйћенеджер
+{
+public:
+	–есурсныйћенеджер (const std::string& ѕутьисполнимый);
+	~–есурсныйћенеджер()= default;
+
+
+	–есурсныйћенеджер(const –есурсныйћенеджер&) = delete;
+	–есурсныйћенеджер& operator=(const –есурсныйћенеджер&) = delete;
+	–есурсныйћенеджер& operator=( –есурсныйћенеджер&&) = delete;
+	–есурсныйћенеджер(–есурсныйћенеджер&&) = delete;
+
+
+	std::shared_ptr< –ендор ::Ўейдерна€ѕрограмма> загрускаЎедоры(const std::string& »м€Ўедора, const std::string& ¬ершинныйѕуть, const std::string& ‘рагментныйѕуть);
+	std::shared_ptr< –ендор::Ўейдерна€ѕрограмма> получатьЎудоровѕрограмм(const std::string& »м€Ўедора);
+
+
+
+private:
+	 std::string получить—троку‘айла(const std::string& относительный‘айлаѕути);
+
+	typedef std::map<const std::string, std::shared_ptr< –ендор::Ўейдерна€ѕрограмма>> Ўейдерна€ѕрограммаѕлан;
+	Ўейдерна€ѕрограммаѕлан п_Ўейдерна€ѕрограмма;
+
+
+	std::string п_ѕуть;
+};
+
